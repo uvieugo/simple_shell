b9e7b901fe54042fdc0ef68f14a4e0ca1c9302e9
cofeedcognac <okesmimi@gmail.com>
2022-11-14 07:23:10 -0800

    add main.c file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+}
