2f29abffcc1b5e02195528bdaf1c1f89860d524f
cofeedcognac <okesmimi@gmail.com>
2022-11-14 07:38:30 -0800

    creating aux.c

--- main.h
+++ main.h
@@ -92,4 +92,11 @@ typedef struct builtin_s
 	int (*f)(data_shell *datash);
 } builtin_t;
 
+/* aux_lists.c */
+sep_list *add_sep_node_end(sep_list **head, char sep);
+void free_sep_list(sep_list **head);
+line_list *add_line_node_end(line_list **head, char *line);
+void free_line_list(line_list **head);
+
+
 #endif
