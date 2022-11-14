18a7b623278217599075f0d83042aafe9ab040b2
cofeedcognac <okesmimi@gmail.com>
2022-11-14 07:32:52 -0800

    cretating aux.c

---
+++ aux_lists.c
@@ -0,0 +1,4 @@
+sep_list *add_sep_node_end(sep_list **head, char sep);
+void free_sep_list(sep_list **head);
+line_list *add_line_node_end(line_list **head, char *line);
+void free_line_list(line_list **head);
