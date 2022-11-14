3891366e31cc6d8327c5d978941ce339118b6af0
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2022-11-14 16:18:03 +0100

    Add main header file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+#endif
